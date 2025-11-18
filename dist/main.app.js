4535934aebc26df73ab98a22329482cbbecaac0d
Wandimayew <wandimayew8@gmail.com>
2025-11-18 10:30:06 +0300

    fixing main  for startup 2

--- dist/main.app.js
+++ dist/main.app.js
@@ -1,12 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
-(0, child_process_1.exec)('npm run start:prod', (err, stdout, stderr) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(stdout);
-    console.error(stderr);
+const services = [
+    'dist/gateway/main.gateway.js',
+    'dist/auth/main.auth.js',
+    'dist/operations/main.operations.js',
+    'dist/fulfillment/main.fulfillment.js',
+    'dist/notification/main.notification.js',
+];
+services.forEach((service) => {
+    const proc = (0, child_process_1.exec)(`node ${service}`);
+    proc.stdout?.on('data', (data) => console.log(`[${service}] ${data}`));
+    proc.stderr?.on('data', (err) => console.error(`[${service}] ${err}`));
 });
 //# sourceMappingURL=main.app.js.map
--- src/main.app.ts
+++ src/main.app.ts
@@ -1,11 +1,16 @@
-// src/main.container.ts
+// src/main.app.ts
 import { exec } from 'child_process';
 
-exec('npm run start:prod', (err, stdout, stderr) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(stdout);
-  console.error(stderr);
+const services = [
+  'dist/gateway/main.gateway.js',
+  'dist/auth/main.auth.js',
+  'dist/operations/main.operations.js',
+  'dist/fulfillment/main.fulfillment.js',
+  'dist/notification/main.notification.js',
+];
+
+services.forEach((service) => {
+  const proc = exec(`node ${service}`);
+  proc.stdout?.on('data', (data) => console.log(`[${service}] ${data}`));
+  proc.stderr?.on('data', (err) => console.error(`[${service}] ${err}`));
 });
